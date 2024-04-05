7051463b71094b4ae7ab0949f8010ebb9a098ace
osmr99 <omarsantiagomorales@gmail.com>
2024-04-05 00:14:37 -0500

    Automatic weapon done

--- Assets/Guns/AutomaticWeapon.cs
+++ Assets/Guns/AutomaticWeapon.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shotgun : Gun
+public class AutomaticWeapon : Gun
 {
-    [SerializeField] GameObject prefabShotgunBlast;
     public override bool AttemptFire()
     {
         if (!base.AttemptFire())
             return false;
 
         var b = Instantiate(bulletPrefab, gunBarrelEnd.transform.position, gunBarrelEnd.rotation);
-        b.GetComponent<Projectile>().Initialize(10, 100, 0.5f, 25, null); // version without special effect
+        b.GetComponent<Projectile>().Initialize(1, 100, 2, 2, null); // version without special effect
 
-        Instantiate(prefabShotgunBlast, gunBarrelEnd.transform.position, transform.rotation);
 
         anim.SetTrigger("shoot");
         elapsed = 0;
